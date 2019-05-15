b97c51492e9900dce9e5a3ffb7da32b6fba189b2
Kyle-OD <kpod@udel.edu>
2019-05-15 11:02:18 -0400

    cleaned gpio setup, fixed pump control, removed unnecessary import

--- sampler.py
+++ sampler.py
@@ -6,63 +6,57 @@
 #################################################################################################################
 from firebase import firebase
 import time
-import serial
 import RPi.GPIO as GPIO
 
 
 def init():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)  # Pin for Motor backwards
-    GPIO.setup(23, GPIO.OUT)  # Pin for Motor forwards
-    GPIO.setup(24, GPIO.OUT)  # Pin for Pump in
-    GPIO.setup(25, GPIO.OUT)  # Pin for Pump out
-    GPIO.setup(27, GPIO.OUT)  # Pin for H-Bridge ENA Signal
+    GPIO.setup(18, GPIO.OUT, initial=False)  # Pin for Motor backwards
+    GPIO.setup(23, GPIO.OUT, initial=False)  # Pin for Motor forwards
+    GPIO.setup(24, GPIO.OUT, initial=False)  # Pin for Pump in
+    GPIO.setup(25, GPIO.OUT, initial=False)  # Pin for Pump out
+    GPIO.setup(27, GPIO.OUT, initial=False)  # Pin for H-Bridge ENA Signal
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Pin for sample button
-    GPIO.output(18, False)
-    GPIO.output(23, False)
-    GPIO.output(24, False)
-    GPIO.output(25, False)
-    GPIO.output(27, False)
 
 
 def motor_backwards():
     # Motor to zero position***************************
-    GPIO.clean()
+    GPIO.cleanup()
     init()
     GPIO.output(18, True)  # Turn motor on
     GPIO.output(23, False)
     GPIO.output(27, True)  # ENA signal on
     time.sleep(1)  # SET TO TIME IT TAKES TO RESET
-    GPIO.output(18, 0)  # Turn motor off
-    GPIO.output(27, 0)  # ENA signal off
+    GPIO.output(18, False)  # Turn motor off
+    GPIO.output(27, False)  # ENA signal off
     # *************************************************
 
 
 def motor_forwards():
     # Motor to down position***************************
-    GPIO.clean()
+    GPIO.cleanup()
     init()
-    GPIO.output(23, 1)  # Turn motor on
-    GPIO.output(27, 1)  # ENA signal on
+    GPIO.output(23, True)  # Turn motor on
+    GPIO.output(27, True)  # ENA signal on
     time.sleep(1)  # SET TO TIME IT TAKES TO GO DOWN
-    GPIO.output(23, 0)  # Turn motor off
-    GPIO.output(27, 0)  # ENA signal off
+    GPIO.output(23, False)  # Turn motor off
+    GPIO.output(27, False)  # ENA signal off
     # *************************************************
 
 
 def pump_sequence():
     # Pump flush --> fill******************************
-    GPIO.clean()
+    GPIO.cleanup()
     init()
-    GPIO.output(18, 1)  # Turn fill pump on
+    GPIO.output(24, True)  # Turn fill pump on
     time.sleep(1)  # SET TO TIME IT TAKES TO FILL
-    GPIO.output(18, 0)  # Turn fill pump off
-    GPIO.output(18, 1)  # Turn flush pump on
+    GPIO.output(24, False)  # Turn fill pump off
+    GPIO.output(25, True)  # Turn flush pump on
     time.sleep(1)  # SET TO TIME IT TAKES TO FLUSH
-    GPIO.output(18, 0)  # Turn flush pump off
-    GPIO.output(18, 1)  # Turn fill pump on
+    GPIO.output(25, False)  # Turn flush pump off
+    GPIO.output(24, True)  # Turn fill pump on
     time.sleep(1)  # SET TO TIME IT TAKES TO FILL
-    GPIO.output(18, 0)  # Turn fill pump off
+    GPIO.output(24, False)  # Turn fill pump off
     # *************************************************
 
 
@@ -71,11 +65,13 @@ def run_sequence():
     motor_forwards()
     pump_sequence()
     motor_backwards()
+    GPIO.cleanup()
+    init()
 
 
 firebase = firebase.FirebaseApplication('https://stream-sampler.firebaseio.com/')  # Set Firebase URL
 time.sleep(3)
-result = '0'
+result = 0
 init()
 
 while True:  # Execution loop
